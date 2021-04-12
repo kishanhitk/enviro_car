ba229483ab83ef017f638d56c7f51ae156220265
kishanhitk <kishansharma1231@gmail.com>
2021-04-12 20:16:25 +0530

    ✨ Add Dashboard Dummy UI

--- lib/view/dashboard_page/dashboard_page.dart
+++ lib/view/dashboard_page/dashboard_page.dart
@@ -6,45 +6,65 @@ import 'package:flutter/material.dart';
 
 class DashboardPage extends StatelessWidget {
   final user = userBox.get('user') as User?;
-  Future<void> _logout() async {
-    await userBox.clear();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: kThemeBlueColor,
-        title: SizedBox(
-          height: 30,
-          child: Image.asset(
-            'assets/img_envirocar_logo_white.png',
-            fit: BoxFit.contain,
-          ),
-        ),
-        actions: [
-          IconButton(
-              icon: Icon(Icons.logout),
-              onPressed: () async {
-                await _logout();
-                Navigator.of(context).pushReplacementNamed('/');
-              })
-        ],
-      ),
       body: Center(
         child: Container(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Text("Welcome ${user!.name}"),
-              Text("Your email is ${user!.mail}"),
+              Container(
+                width: double.maxFinite,
+                color: kThemeBlueColor,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        height: 30,
+                        child: Image.asset(
+                          'assets/img_envirocar_logo_white.png',
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: Icon(
+                          Icons.person,
+                          color: kButtonColor,
+                          size: 80,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: kButtonColor),
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 15.0),
+                      child: Text(
+                        user?.name ?? "User",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Spacer(),
               AuthButton(
                 onPressed: () async {
                   Navigator.of(context).pushNamed('/bluetooth');
                 },
                 label: "Bluetooth Page",
-              )
+              ),
+              Spacer(),
             ],
           ),
         ),
